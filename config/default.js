f4a0eb6824f52618db8e14fc425e7f0637898e90
Teo Oliver <teo.liz.oli@gmail.com>
2019-08-28 22:31:26 +0100

    set config

---
+++ config/default.js
@@ -0,0 +1,4 @@
+module.exports = {
+  mongoURI:
+    'mongodb+srv://teo:12b425@retrace-pbkpl.mongodb.net/test?retryWrites=true&w=majority'
+};
---
+++ models/Shift.js
@@ -0,0 +1,6 @@
+// name;
+// clock_in;
+// clock_out;
+// date;
+// duration;
+// break_time;
